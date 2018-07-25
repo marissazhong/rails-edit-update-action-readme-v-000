1feca8ff1ae6e67b352362e53ecc06e4721f59f9
Marissa Zhong <mwz2101@columbia.edu>
2018-07-25 01:04:54 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,5 +27,6 @@ class PostsController < ApplicationController
 	def update
 		@post = Post.find(params[:id])
 	  @post.update(title: params[:title], description: params[:description])
-	  redirect_to post_path(@post)	end
+	  redirect_to post_path(@post)
+	end
 end
